6a79b1314686c01e1f272de8ecfd153816ed99ac
cesar <cdma.86@hotmail.com>
2015-09-15 20:11:13 +0200

    blablabla

--- chess-game/src/main/java/chess/Board.java
+++ chess-game/src/main/java/chess/Board.java
@@ -8,16 +8,44 @@ import java.util.List;
  */
 public class Board {
 
+    private Cell[][] cells;
+    private int n;
+    private int m;
+
+    public Board(int m, int n) {
+        this.n = n;
+        this.m = m;
+
+        cells = new Cell[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                cells[i][j] = new Cell();
+            }
+        }
+    }
+
+    public static Board emptyChessBoard() {
+        return new Board(8, 8);
+    }
+
     public boolean isValid(Position pos) {
         return isValid(pos.getI(), pos.getJ());
     }
 
     public boolean isValid(int i, int j) {
-        return i>=0 && i<8 && j>=0 && j<8;
+        return isInRange(i, j);
+    }
+
+    public boolean isInRange(Position pos) {
+        return isInRange(pos.getI(), pos.getJ());
+    }
+
+    public boolean isInRange(int i, int j) {
+        return i>=0 && i<m && j>=0 && j<n;
     }
 
     public boolean isEmpty(int i, int j) {
-        return true;
+        return cells[i][j].isEmpty();
     }
 
     public List<Position> searchByIncrement(Position pos, int incI, int incJ) {
